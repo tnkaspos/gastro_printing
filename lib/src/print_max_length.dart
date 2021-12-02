f138a9bb04cc304b00e4f25a2fc63a0e8e191295
DucPhan13 <minhducphan13@gmail.com>
2021-12-02 19:09:52 +0700

    reinitial branch

---
+++ lib/src/print_max_length.dart
@@ -0,0 +1,10 @@
+class PrintMaxLength {
+  int quantity;
+  int itemID;
+  int single;
+  int total;
+  int lineA;
+  int lineB;
+
+  PrintMaxLength(this.quantity, this.itemID, this.single, this.total, this.lineA, this.lineB);
+}
